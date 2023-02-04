763925e70500da3630976911114f51f2154dc7bf
SlavONYX <slavonixxx@gmail.com>
2023-02-04 23:29:58 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,124 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react'
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Searchbar } from "./Searchbar/Searchbar";
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal'
+import axios from "axios";
+import styles from './App.module.css'
+import Notiflix from 'notiflix';
+
+
+export class App extends Component {
+
+  state = {
+    URL: 'https://pixabay.com/api/',
+    KEY: '29185348-64a39df69b18d57fec00c3d74',
+    cards: [],
+    search: "",
+    error: "",
+    loading: false,
+    page: 1,
+    showModal: false,
+    modalImage: null,
+  }
+
+  fetchPosts = () => {
+    const { search, page, URL, KEY } = this.state
+    
+    axios.get(`${URL}?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+    .then(response => response.data.hits)
+    .then(data => {
+      const dataArray = [];
+      data.map(({ id, webformatURL, largeImageURL }) =>dataArray.push({ id, webformatURL, largeImageURL })
+      )
+      if (dataArray.length === 0) {
+        Notiflix.Notify.failure('not found any picture!');
+      }
+      return dataArray
+    }
+    )
+    .then( (newCards) => {
+        this.setState((prevState) => {
+          if (prevState.cards.length === 0) {
+        return {
+        cards: newCards,
+      }
+      } else {
+        
+        return {
+          cards: [...prevState.cards, ...newCards]
+        }
+      }
+      
+      })
+    })
+    .catch(error => {
+      this.setState({
+        error
+      })
+    })
+      .finally(() => this.setState({
+        loading: false,  
+      })
+      )
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.searchInput.value
+    if (searchValue !== "" && searchValue !== this.state.search) {
+      this.setState({
+      cards: [],
+      search: searchValue,
+      page: 1,
+      loading: true,
+      
+    })
+    } else if (searchValue === "") {
+      Notiflix.Notify.info('input is empty!');
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.search !== prevState.search || this.state.page !== prevState.page) {
+      setTimeout(this.fetchPosts, 200) 
+    }
+  }
+
+  onLoadMore = () => {
+    this.setState((prevState) => {
+      return {
+        page: prevState.page + 1,
+        loading: true,
+      }
+    })
+  }
+  
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal, }));
+  }
+  
+  openModal = (largeImageURL) => {
+    this.setState({
+      modalImage: largeImageURL,
+    })
+    this.toggleModal()
+
+  }
+
+  render() {
+    const {showModal,modalImage,cards,loading} = this.state
+    return (
+      <div className={styles.app}>
+        <Searchbar onSubmit={this.onSubmit} />
+        <ImageGallery cards={cards} onOpen={this.openModal} />
+        {loading && <Loader/>}
+        {cards.length > 1 && <Button onLoadMore={this.onLoadMore} />}
+        {showModal && modalImage && (<Modal onClose={this.toggleModal} modalImage={modalImage} />)}
+      </div>
+    );
+  }
+  
 };
